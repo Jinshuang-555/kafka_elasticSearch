5a48b09be71701b378027a0c0ad8d85739d2475b
Niu Jinshuang <niujinshuang@Nius-MacBook-Pro.local>
2023-03-30 14:19:35 -0700

    update kfaka server

--- src/main/java/com/example/kafka_elasticsearch/kafkaConsumer/KafkaConsumerConfig.java
+++ src/main/java/com/example/kafka_elasticsearch/kafkaConsumer/KafkaConsumerConfig.java
@@ -43,7 +43,7 @@ public class KafkaConsumerConfig {
     @Bean
     public Map<String, Object> consumerConfigs() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, boostrapServers);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "group_id");
